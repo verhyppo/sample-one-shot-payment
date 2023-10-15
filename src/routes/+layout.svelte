8b9d2280d93c06dc8904a7b26d251b585e1976d2
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-15 13:21:26 +0200

    1 step at a time

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script>
   import {config} from "../staticConf"
-  import "../app.css"
+  import {onMount} from 'svelte';
+  import TopAppBar, {Row, Section, Title, AutoAdjust} from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+
+  let darkTheme = true;
+  let topAppBar;
+
+  $: modeLabel = `switch to ${darkTheme ? 'light' : 'dark'} mode`;
+
+  // This icon represents the mode to which the user can switch.
+  $: modeIcon = darkTheme ? 'light_mode' : 'dark_mode';
+
+  onMount(() => {
+    darkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  });
+
+  const toggleMode = () => (darkTheme = !darkTheme);
 
   export let generateMicrosoftLoginUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?\
             client_id=719d34c4-8b45-40c2-b436-bbcb176580a4\
@@ -11,13 +27,88 @@
             &state=12345\
             &code_challenge=${config.challenge}\
             &code_challenge_method=S256`;
+
 </script>
 
-<nav>
-  <a href="/">Home</a>
-  <a href="/about">About</a>
-  <a href="/settings">Settings</a>
-  <a href={generateMicrosoftLoginUrl}>Signin</a>
-</nav>
+<style>
+ 
+ .top-app-bar-container {
+    width: 100%;
+    height:100%;
+    background-color: var(--mdc-theme-background, #fff);
+    overflow: auto;
+    display: inline-block;
+  }
+ 
+  @media (max-width: 480px) {
+    .top-app-bar-container {
+      margin-right: 0;
+    }
+  }
+ 
+  .flexy {
+    display: flex;
+    flex-wrap: wrap;
+    height:100%;
+  }
+ 
+  .flexor {
+    display: inline-flex;
+    flex-direction: column;
+  }
+ 
+  .flexor-content {
+    flex-basis: 0;
+    height: 0;
+    flex-grow: 1;
+    overflow: auto;
+  }
+</style>
 
-<slot />
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="/global.css"
+  />
+  {#if darkTheme === undefined}
+  <link
+    rel="stylesheet"
+    href="/smui.css"
+    media="(prefers-color-scheme: light)"
+  />
+  <link
+    rel="stylesheet"
+    href="/smui-dark.css"
+    media="screen and (prefers-color-scheme: dark)"
+  />
+  {:else if darkTheme}
+  <link rel="stylesheet" href="/smui.css" media="print" />
+  <link rel="stylesheet" href="/smui-dark.css" media="screen" />
+  {:else}
+  <link rel="stylesheet" href="/smui.css" />
+  {/if}
+</svelte:head>
+<div class="flexy">
+  <div class="top-app-bar-container flexor">
+    <TopAppBar bind:this="{topAppBar}" variant="static">
+      <Row>
+        <Section>
+          <IconButton class="material-icons">menu</IconButton>
+          <Title>Standard</Title>
+        </Section>
+        <Section align="end" toolbar>
+          <IconButton
+            aria-label="{modeLabel}"
+            class="material-icons"
+            on:click="{toggleMode}"
+            title="{modeLabel}"
+          >
+            {modeIcon}
+          </IconButton>
+        </Section>
+      </Row>
+    </TopAppBar>
+      <slot />
+  </div>
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,86 @@
-<h1>Home</h1>
+<LayoutGrid>
+    <Cell span={3}>
+        <div class="demo-cell"></div>
+    </Cell>
+
+    <Cell span={6}>
+<div class="card-display">
+
+    <div class="card-container">
+        <Card>
+          <PrimaryAction on:click={() => clicked++}>
+            <Media class="card-media-16x9" aspectRatio="16x9">
+              <MediaContent>
+                <div
+                  style="color: #fff; position: absolute; bottom: 16px; left: 16px;"
+                >
+                  <h2 class="mdc-typography--headline6" style="margin: 0;">
+                    A card with media.
+                  </h2>
+                  <h3 class="mdc-typography--subtitle2" style="margin: 0;">
+                    And a subtitle.
+                  </h3>
+                </div>
+              </MediaContent>
+            </Media>
+            <Content class="mdc-typography--body2">
+              It's all in this card. It's a veritable smorgasbord of card features.
+            </Content>
+          </PrimaryAction>
+          <Actions>
+            <ActionButtons>
+              <Button on:click={() => clicked++}>
+                <Label>Action</Label>
+              </Button>
+              <Button on:click={() => clicked++}>
+                <Label>Another</Label>
+              </Button>
+            </ActionButtons>
+            <ActionIcons>
+              <IconButton
+                on:click={() => clicked++}
+                toggle
+                aria-label="Add to favorites"
+                title="Add to favorites"
+              >
+                <Icon class="material-icons" on>favorite</Icon>
+                <Icon class="material-icons">favorite_border</Icon>
+              </IconButton>
+              <IconButton
+                class="material-icons"
+                on:click={() => clicked++}
+                title="Share">share</IconButton
+              >
+              <IconButton
+                class="material-icons"
+                on:click={() => clicked++}
+                title="More options">more_vert</IconButton
+              >
+            </ActionIcons>
+          </Actions>
+        </Card>
+      </div>
+</div>
+</Cell>
+<Cell span={3}>
+    <div class="demo-cell"></div>
+</Cell>
+
+</LayoutGrid> 
+<script lang="ts">
+  import Card, {
+    Content,
+    PrimaryAction,
+    Media,
+    MediaContent,
+    Actions,
+    ActionButtons,
+    ActionIcons,
+  } from '@smui/card';
+  import Button, { Label } from '@smui/button';
+  import IconButton, { Icon } from '@smui/icon-button';
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+
+ 
+  let clicked = 0;
+</script>
