adcc086f4a01571553a9105df5a3d79d1523f531
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-19 14:01:18 +0200

    variable name typo

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 import { env } from '$env/dynamic/private';
-export const supabase = createClient('https://rqwwaedoaszvqyjrcuro.supabase.co', env.SUPABASENEXT_PUBLIC_SUPABASE_ANON_KEY_APIKEY)
+export const supabase = createClient('https://rqwwaedoaszvqyjrcuro.supabase.co', env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
