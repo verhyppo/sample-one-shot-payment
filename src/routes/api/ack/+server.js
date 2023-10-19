0906d4daaa1a5de33b18b8990e9f202836877c1e
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-19 21:25:45 +0200

    fix on ack endpoint

--- src/routes/api/ack/+server.js
+++ src/routes/api/ack/+server.js
@@ -3,6 +3,6 @@ import {upsertOrder} from "$lib/server/database/orderrepository"
 export async function POST(event) {
   const data = await event.request.json();
   console.log(data);
-  upsertOrder(data.order.orderId, data, true);
+  upsertOrder(data.operation.orderId, data, true);
   return new Response(JSON.stringify({ message: "CIAO BRUS! CIAO EMA!" }), { status: 200 }) 
 }
