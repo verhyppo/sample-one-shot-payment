c08fa7c9227ef65927609040cd22c04d1afe161e
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-19 23:28:29 +0200

    :)

--- src/routes/api/pbl/+server.js
+++ src/routes/api/pbl/+server.js
@@ -3,7 +3,7 @@ import payService from "$lib/server/payService";
 
 export async function GET({ url }) {
   let amount = url.searchParams.get("amount");
-  return payService
+  return payService()
     .pbl(url.origin, amount)
     .then((json) => new Response(JSON.stringify(json)))
     .catch((e) => {
