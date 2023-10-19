1ff79396ba1e182cbb9ff8b1cda8fe7a10acce6d
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-19 21:39:23 +0200

    upsert?

--- src/lib/server/database/orderrepository.js
+++ src/lib/server/database/orderrepository.js
@@ -1,11 +1,17 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function saveOrder(correlationId, order) {
-  const { data, error } = await supabase.from("orders").insert({ order_id: order.order.orderId, trace_id: correlationId, order: order})
+  const { data, error } = await supabase.from("orders").insert({
+    order_id: order.order.orderId,
+    trace_id: correlationId,
+    order: order,
+  });
   console.log("data", data, error);
 }
 export async function upsertOrder(orderId, response, ack) {
   const { data, error } = await supabase
-  .from('orders')
-  .upsert({ orderId: orderId, response: response, ack: true }, { onConflict: 'orderId' }) 
+    .from("orders")
+    .upsert({ orderId: orderId, response: response, ack: true })
+    .eq("orderId", orderId);
+  console.log(data, error);
 }
