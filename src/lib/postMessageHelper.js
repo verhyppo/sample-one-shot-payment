48dd830b3f9a3f0feeff4003a6aa5a26bc1f8dfd
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-21 18:05:53 +0200

    - handle better postmessages

--- src/lib/postMessageHelper.js
+++ src/lib/postMessageHelper.js
@@ -1,39 +1,44 @@
 import { store } from "$lib/store/cardDataStore";
 import { iframePostMessageUrl } from "$lib/client.config.js";
 import { goto } from "$app/navigation";
-import notifyErrors from '$lib/store/errorStore'
+import notifyErrors from "$lib/store/errorStore";
+
+const manageStateChange = async (event) => {
+  const { state } = event; //handle all events
+  switch (state) {
+    case "CARD_DATA_COLLECTION":
+      store.setFields(event.fieldSet.fields);
+      goto("/build/carddata");
+      break;
+    case "READY_FOR_PAYMENT":
+      store.setReadyForPayment();
+      //location.href = "finalize_hpp.html";
+      break;
+    default:
+      console.log(`${state} does not match with any handle. Bail.`);
+      break;
+  }
+};
 export default (evt) => {
   console.log("Event", evt);
   if (evt.origin.indexOf(iframePostMessageUrl) === 0) {
-    let state;
-    if ("state" in evt.data) {
-      state = evt.data.state;
-    } else if ("data" in evt.data && "state" in evt.data.data) {
-      state = evt.data.data.state;
-    }
+    let data = evt || evt.data.data || evt.data || [];
 
-    if (state) {
-      //handle all events
-      switch (state) {
-        case "CARD_DATA_COLLECTION":
-          store.setFields(evt.data.data.fieldSet.fields);
-          goto("/build/carddata");
-          break;
-        case "READY_FOR_PAYMENT":
-          store.setReadyForPayment();
-          break;
-        default:
-          location.href = "finalize_hpp.html";
-          break;
-      }
+    const { event, id, message } = data;
+    switch (event) {
+      case "BUILD_FLOW_STATE_CHANGE":
+        manageStateChange(data);
+        break;
+      case "BUILD_ERROR":
+        return notifyErrors(`${id} launched an error ${message}`);
+        break;
+      case "BUILD_SUCCESS":
+        console.log(`${id} saying something good happened`);
+        break;
+      default:
+        console.log(`${id} saying ${event} happened. Nothing specific to do.`);
     }
-    if (
-      "event" in evt.data &&
-      evt.data["event"] === "BUILD_ERROR" &&
-      "id" in evt.data &&
-      evt.data["id"]
-    ) {
-      notifyErrors(`An error occurred while processing payment: ${evt.data["id"]} - ${evt.data["errorCode"]}: ${evt.data["errorMessage"]}`)
-    }
+  } else {
+    console.error(`Received a message we don't want to receive! ${evt.origin}`);
   }
 };
