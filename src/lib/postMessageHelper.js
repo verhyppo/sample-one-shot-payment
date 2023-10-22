1dcae67fd67d4408a1e384786a5c8649dc7458eb
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-22 12:53:27 +0200

    - fix event handler to return appropriate data

--- src/lib/postMessageHelper.js
+++ src/lib/postMessageHelper.js
@@ -21,8 +21,8 @@ const manageStateChange = async (event) => {
 };
 export default (evt) => {
   console.log("Event", evt);
-  if (evt.origin.indexOf(iframePostMessageUrl) === 0) {
-    let data = evt || "data" in evt.data ? evt.data.data : evt.data;
+  if (evt?.origin?.indexOf(iframePostMessageUrl) === 0) {
+    let data = "data" in evt.data ? evt.data.data : evt.data;
 
     const { event, id, message } = data;
     console.log("logging extracted data", data, event, id, message)
