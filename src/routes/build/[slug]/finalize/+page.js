8cbc8ea78ac511e0dc5b076d780227b3bb176c77
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-22 16:41:33 +0200

    href should be enough

--- src/routes/build/[slug]/finalize/+page.js
+++ src/routes/build/[slug]/finalize/+page.js
@@ -1,14 +1,19 @@
-import { error } from "@sveltejs/kit";
-import { store } from "$lib/store/buildFlowStore";
+import {error} from "@sveltejs/kit";
 
 /** @type {import('./$types').PageLoad} */
 
-export async function load({ params, url, fetch }) {
-  const orderId = params.slug;
-  const json = await fetch(`/api/order/${orderId}`)
-    .then((data) => data.json())
-    .catch((e) => {
-      throw error(400, e);
-    });
-  return json;
+export async function load({params, url, fetch}) {
+    const orderId = params.slug;
+    console.log("In load function")
+    const json = await fetch(`/api/order/${orderId}`)
+        .then((data) => {
+            console.log(data.status);
+            data.json()
+        })
+        .catch((e) => {
+            console.log(e);
+            throw error(400, e);
+        });
+    console.log(json)
+    return json;
 }
