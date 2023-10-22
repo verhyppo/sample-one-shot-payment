0c6a950811210da00b9e69724362187b0b34f968
Veronica Di Giorgio <veronica.digiorgio@konicaminolta.it>
2023-10-22 10:51:26 +0200

    if you import stuff, they usually work :)

--- src/routes/build/[slug]/finalize/+page.svelte
+++ src/routes/build/[slug]/finalize/+page.svelte
@@ -1,13 +1,14 @@
 <script>
   import { store } from "$lib/store/buildFlowStore";
+  import { goto } from "$app/navigation";
   import Button from "@smui/button";
 
   /**
-   * in this case we are opening a new page, but you could complete
+   * TIP: in this case we are opening a new page, but you could complete
    * your order in a Dialog, or whatever you prefer!
-  */
-   /** @type {import('./$types').PageData} */
+   */
 
+  /** @type {import('./$types').PageData} */
   export let data;
 
   const pay = async () => {
@@ -32,13 +33,18 @@
 <div class="payment-form" id="payment-methods">
   <h1 class="payment-title">Your payment recap</h1>
   <div class="payment-content">
-
     <div>
       <h2>Thanks for buying at PICCOL E-SHOP</h2>
-      <h3> your goods will arrive in 365 days directly at the address you haven't communicated</h3>
-      <h3> Let's complete your payment </h3>
-      <p>Would you like to complete the payment of {data.orderStatus.order.currency} {parseInt(data.orderStatus.order.amount)/100}?</p>
-      
+      <h3>
+        your goods will arrive in 365 days directly at the address you haven't
+        communicated
+      </h3>
+      <h3>Let's complete your payment</h3>
+      <p>
+        Would you like to complete the payment of {data.orderStatus.order
+          .currency}
+        {parseInt(data.orderStatus.order.amount) / 100}?
+      </p>
     </div>
     <Button on:click={() => pay()} variant="raised" color="secondary"
       >I'm ready! Let's move on</Button
