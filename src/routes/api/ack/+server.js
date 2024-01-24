d6dddb751ae4fbe4eacd9ec313209f71169a26e4
Veronica <3539384+verhyppo@users.noreply.github.com>
2024-01-24 17:36:42 +0100

    log data as it sounds weird

--- src/routes/api/ack/+server.js
+++ src/routes/api/ack/+server.js
@@ -1,6 +1,7 @@
 import { upsertOrder } from "$lib/server/database/orderrepository";
 export async function POST(event) {
   const data = await event.request.json();
+  console.log(data);
   return upsertOrder(data.operation.orderId, data, true).then(
     () =>
       new Response(JSON.stringify({ message: "CIAO BRUS! CIAO EMA!" }), {
